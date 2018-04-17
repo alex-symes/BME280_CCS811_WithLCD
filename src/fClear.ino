a12f1b8771fee85d0bb3d9d3abc4f735fc77b150
alex-symes <34386680+alex-symes@users.noreply.github.com>
2018-04-16 20:13:54 -0400

    Update and rename fClear.ino to src/fClear.ino

---
+++ src/fClear.ino
@@ -0,0 +1,20 @@
+void fClear(void) {
+        
+          delay(displayDelay);
+          lcd.LCD_clear(); // blanks the display
+          delay(500);
+          lcd.LCD_write_string(0, 1, "Clearing ", MENU_NORMAL);
+          lcd.LCD_write_string(0, 2, "EEPROM ", MENU_NORMAL);
+           delay(1000);
+           lcd.LCD_clear(); // blanks the display
+            EEPROM.write(0, 0x00);
+            EEPROM.write(1, 0x00);
+            EEPROM.write(2, 0x00);
+            EEPROM.write(3, 0x00);
+
+
+   
+  }
+
+
+  
